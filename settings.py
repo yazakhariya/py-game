25b4ed405cccb9ac810e705dd7cec0f88978803b
yazakhariya <17070501.iana@gmail.com>
2025-04-14 22:39:52 +0300

    feature: speed change

--- settings.py
+++ settings.py
@@ -4,16 +4,20 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        self.ship_speed = 5
+        self.speedpu_scale = 2
         self.ship_limit = 3
+        self.initialize_dynamic_settings()
 
-        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
 
-        self.alien_speed = 2.5
         self.fleet_drop_speed = 10
-        self.fleet_direction = 1                  # moving to the right, -1 for a left moving
         
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 5
+        self.bullet_speed = 5.0
+        self.alien_speed = 2.5
+        self.fleet_direction = 1                  # moving to the right, -1 for a left moving
